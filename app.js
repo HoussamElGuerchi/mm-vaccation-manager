8eb96735c480c148ca9d14b0cf4c7e28fa034c08
Houssam EL GUERCHI <houssamelgu@gmail.com>
2020-02-12 12:29:21 +0100

    Create initial project skeleton and complete authentication page

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// Authentication Page
+app.get("/authent", (req, res) => {
+    res.render("authent", {pageTitle: "Authentification"});
+});
+
+// Home page
+app.get("/acceuil", (req, res) => {
+    res.render("index", {pageTitle: "Acceuil"});
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+})
