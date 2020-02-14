958fe891fa072e843fe1be1cd09ce00ac6c0c3ca
Houssam EL GUERCHI <houssamelgu@gmail.com>
2020-02-14 11:56:24 +0100

    complete form page

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+/***** Variables & functions Declaration *****/
+
+/*********************************/
+
 // Authentication Page
 app.get("/authent", (req, res) => {
     res.render("authent", {pageTitle: "Authentification"});
@@ -23,6 +27,11 @@ app.get("/", (req, res) => {
     // res.render("index", {pageTitle: "Acceuil"});
     res.redirect("/authent");
 });
+
+//Vacation form
+app.get("/nouveau-conge", (req, res) => {
+    res.render("leave-form", {pageTitle: "Nouveau Congé"});
+})
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
